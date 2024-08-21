45d3dfa8150d538012457b6b9553c1820c5dbb59
Guillermo Gonzalez <guillermo.gonzalezt@correo.ucu.edu.uy>
2024-08-21 08:42:47 -0300

    clase dish creada

--- src/Dish.cs
+++ src/Dish.cs
@@ -5,5 +5,53 @@ namespace Ucu.Poo.Restaurant;
 /// </summary>
 public class Dish
 {
-    
+    public string name;
+    public string Name(value)       //nombre del platillo
+    {
+        get {
+            return name;
+        }
+        set
+        {
+            name = value;
+        }
+    }
+
+    public int price;
+    public int Price(value)     //precio del platillo
+    {
+        get {
+            return price;
+        }
+        set
+        {
+            price = value;
+        }
+    }
+
+    public bool vegetarian;
+    public string IsVegetarian(value)    //es vegetariano?  el valor 'vegetariano' es booleano, si es true IsVegetarian devuelve "yes"
+    {                                       // si es false devuelve "no"
+        get {
+            return vegetarian;
+        }
+        set
+        {
+            if (vegetarian)
+            {
+                return "yes";
+            }
+            else
+            {
+                return "no";
+            }
+        }
+    }
+
+    public Dish(string name, int price, bool vegetarian)      //constructor
+    {
+        this.Name = name;
+        this.Price = price;
+        this.IsVegetarian = vegetarian;
+    }
 }
