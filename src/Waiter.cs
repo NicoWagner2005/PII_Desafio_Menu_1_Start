a3786cdfa860219779292b23a55e97c009d14f88
JuanpePena <juanpedro.pena@correo.ucu.edu.uy>
2024-08-21 10:13:21 -0300

    waiter

--- src/Waiter.cs
+++ src/Waiter.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Runtime.CompilerServices;
 
 namespace Ucu.Poo.Restaurant;
 
@@ -8,4 +9,62 @@ namespace Ucu.Poo.Restaurant;
 public class Waiter
 {
     private ArrayList assignedTables = new ArrayList();
+    public ArrayList AssignedTables
+    {
+        get {return assignedTables;}
+        set { assignedTables.Add(value);
+    }
+}
+
+    private string name;
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+
+//Constructor??????!?!?!/!!?!?!?!?!?!?!!!?!!?!?!?
+    public Waiter(string name, ArrayList assignedTables)
+    {
+        this.Name = name;
+        this.AssignedTables = assignedTables;
+    }
+
+    public void AssignTables(Table);
+    {
+        assignedTables.Add(Table);
+    }
+
+    public void TakeOrder(Table, Dish)
+    {
+        Table.AddToOrder(Dish);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
