ad2e19ce1230e3f5cab8dcd7d98fb8f4688d347a
NicoWagner2005 <nicolas.wagner@correo.ucu.edu.uy>
2024-08-21 09:15:48 -0300

    Clase menu creada

--- src/Menu.cs
+++ src/Menu.cs
@@ -9,4 +9,33 @@ using System.Collections;
 public class Menu
 {
     private ArrayList dishes = new ArrayList();
+
+    private ArrayList AddDish(string dish)
+    {
+        dishes.add(dish);
+    }
+
+    private ArrayList RemoveDish(string dish)
+    {
+        dishes.RemoveAt(dishes.IndexOf(dish));
+    }
+
+    private Dish GetDishByName(string dish)
+    {
+        if (dish in dishes)
+        {
+            return dishes[dishes.IndexOf(dish)];
+        }
+        else
+        {
+            return null
+        }
+    }
+
+    public Menu(ArrayList dishes)
+    {
+        this.Dishes = dishes;
+    }
+    
+    
 }
